cbbfcfcaffc746a14a488cfaf67a300cc3b5ef10
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-17 11:10:36 +0200

    Initial commit

---
+++ src/main/java/com/example/randombook/RandombookApplication.java
@@ -0,0 +1,13 @@
+package com.example.randombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandombookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandombookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/randombook/RandombookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randombook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandombookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
