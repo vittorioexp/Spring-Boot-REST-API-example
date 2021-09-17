93996d2e254dbec0f4f1529d863170bb729254a3
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-17 14:46:32 +0200

    preparing main java class

--- src/main/java/com/example/randombook/RandomBookApplication.java
+++ src/main/java/com/example/randombook/RandomBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RandombookApplication {
+public class RandomBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RandombookApplication.class, args);
+		SpringApplication.run(RandomBookApplication.class, args);
 	}
 
 }
