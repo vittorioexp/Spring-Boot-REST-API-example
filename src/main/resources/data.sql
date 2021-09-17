2dd5bab7dbc44a971af6be1397b4435cbdc3ef12
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-17 14:45:04 +0200

    populating db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO category (id_category, name) VALUES
+(1, 'Entrepreneurship'),
+(2, 'Science'),
+(3, 'Economics'),
+(4, 'Investments'),
+(5, 'Relationships'),
+(6, 'Parenting'),
+(7, 'Philosophy'),
+(8, 'Culture'),
+(9, 'Politics'),
+(10, 'Nutrition'),
+(11, 'History'),
+(12, 'Management'),
+(13, 'Psychology');
+
+
+INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
+    (1, 'Blitzscaling', 'Reid Hoffman', 'QuickRead', '987654321', 1, 'https://images-na.ssl-images-amazon.com/images/I/81t-86ISGuL.jpg');
+
+INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
+    (2, 'Company of One', 'Paul Jarvis', 'QuickRead', '987654311', 1, 'https://m.media-amazon.com/images/I/41Rz8Me31NL.jpg');
+
+INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
+    (3, 'Cooking up a business', 'David Cohen and Brad Feld', 'QuickRead', '987654211', 1, 'https://m.media-amazon.com/images/I/519wTAlmTCL.jpg');
+
+INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
+    (4, 'Breakpoint', 'Jeff Stibel', 'QuickRead', '947654211', 2, 'https://images-na.ssl-images-amazon.com/images/I/51B4-igcYsL._AC_UL600_SR600,600_.jpg');
+
+INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
+    (5, 'Conscious', 'Annaka Harris', 'QuickRead', '947354211', 2, 'https://images-na.ssl-images-amazon.com/images/I/91Oy00Se3YL.jpg');
