886e07658bd5ac753d5a9de2544a35a404f30082
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-18 08:04:14 +0200

    writing Book dao interface

--- src/main/java/com/example/randombook/book/BookDAO.java
+++ src/main/java/com/example/randombook/book/BookDAO.java
@@ -1,5 +1,11 @@
 package com.example.randombook.book;
 
 public interface BookDAO {
-
+    Book findById(int id_book);
+    List<Book> findAll();
+    List<Book> findAllByCategory(Category category);
+    Book create(Book book);
+    Book update(Book book);
+    void delete(int id_book);
+    long count();
 }
