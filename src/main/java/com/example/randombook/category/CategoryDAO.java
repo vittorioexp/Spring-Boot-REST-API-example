2718f855c29599c6edec85f5bdb6f582ec990d3e
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-18 08:40:42 +0200

    writing category dao

--- src/main/java/com/example/randombook/category/CategoryDAO.java
+++ src/main/java/com/example/randombook/category/CategoryDAO.java
@@ -3,9 +3,10 @@ package com.example.randombook.category;
 import com.example.randombook.book.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryDAO {
-    Category findById(int id_category);
+    Optional<Category> findById(int id_category);
     List<Category> findAll();
     Category create(Category category);
     Category update(Category category);
--- src/main/java/com/example/randombook/category/JdbcCategoryDAO.java
+++ src/main/java/com/example/randombook/category/JdbcCategoryDAO.java
@@ -1,35 +1,80 @@
 package com.example.randombook.category;
 
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import com.example.randombook.category.*;
+
+import java.util.*;
 
 public class JdbcCategoryDAO implements CategoryDAO {
+
+    private List<Category> categories;
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert insertCategory;
+
+    public JdbcCategoryDAO(JdbcTemplate jdbcTemplate) {
+        this.categories = new ArrayList<Category>();
+        this.jdbcTemplate = jdbcTemplate;
+        insertCategory = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("category")
+                .usingGeneratedKeyColumns("id_category");
+    }
+
+    RowMapper<Category> rowMapper = (rs, rowNum) -> {
+        Category c = new Category(
+                rs.getInt("id_category"),
+                rs.getString("name")
+        );
+        return c;
+    };
+
     @Override
-    public Category findById(int id_category) {
+    public Optional<Category> findById(int id_category) {
+        String sql = "SELECT * FROM category where id_category = ?";
+        // TODO: fix error here
+        //return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, id_category));
         return null;
     }
 
     @Override
     public List<Category> findAll() {
-        return null;
+        String sql = "SELECT * FROM category";
+        return jdbcTemplate.query(sql, rowMapper);
     }
 
     @Override
     public void delete(int id_category) {
-
+        jdbcTemplate.update("delete from category where id_category = ?", id_category);
     }
 
     @Override
     public long count() {
-        return 0;
+        return jdbcTemplate.queryForObject("select count(*) from category", Long.class);
     }
 
     @Override
     public Category update(Category category) {
-        return null;
+        String sql = "update category set name = ? where id_category = ?";
+        jdbcTemplate.update(
+                sql,
+                category.getName(),
+                category.getIdCategory()
+        );
+        return new Category(
+                category.getIdCategory(),
+                category.getName()
+        );
     }
 
     @Override
     public Category create(Category category) {
-        return null;
+        Map<String,Object> parameters = new HashMap<>();
+        parameters.put("name", category.getName());
+        Number id_category = insertCategory.executeAndReturnKey(parameters);
+        return new Category(
+                (Integer) id_category,
+                category.getName()
+        );
     }
 }
