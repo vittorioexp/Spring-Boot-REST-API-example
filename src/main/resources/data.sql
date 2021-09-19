ac650d184ee4487968a49014077d8be88260df7a
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-19 10:22:03 +0200

    fixed sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,17 +14,17 @@ INSERT INTO category (id_category, name) VALUES
 (13, 'Psychology');
 
 
-INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
-    (1, 'Blitzscaling', 'Reid Hoffman', 'QuickRead', '987654321', 1, 'https://images-na.ssl-images-amazon.com/images/I/81t-86ISGuL.jpg');
+INSERT INTO book (id_book, title, author, release_date, isbn, id_category, images) VALUES
+    (1, 'Blitzscaling', 'Reid Hoffman', '2021-09-19', '987654321', 1, 'https://images-na.ssl-images-amazon.com/images/I/81t-86ISGuL.jpg');
 
-INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
-    (2, 'Company of One', 'Paul Jarvis', 'QuickRead', '987654311', 1, 'https://m.media-amazon.com/images/I/41Rz8Me31NL.jpg');
+INSERT INTO book (id_book, title, author, release_date, isbn, id_category, images) VALUES
+    (2, 'Company of One', 'Paul Jarvis', '2021-09-20', '987654311', 1, 'https://m.media-amazon.com/images/I/41Rz8Me31NL.jpg');
 
-INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
-    (3, 'Cooking up a business', 'David Cohen and Brad Feld', 'QuickRead', '987654211', 1, 'https://m.media-amazon.com/images/I/519wTAlmTCL.jpg');
+INSERT INTO book (id_book, title, author, release_date, isbn, id_category, images) VALUES
+    (3, 'Cooking up a business', 'David Cohen and Brad Feld', '2021-09-18', '987654211', 1, 'https://m.media-amazon.com/images/I/519wTAlmTCL.jpg');
 
-INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
-    (4, 'Breakpoint', 'Jeff Stibel', 'QuickRead', '947654211', 2, 'https://images-na.ssl-images-amazon.com/images/I/51B4-igcYsL._AC_UL600_SR600,600_.jpg');
+INSERT INTO book (id_book, title, author, release_date, isbn, id_category, images) VALUES
+    (4, 'Breakpoint', 'Jeff Stibel', '2021-09-18', '947654211', 2, 'https://images-na.ssl-images-amazon.com/images/I/51B4-igcYsL._AC_UL600_SR600,600_.jpg');
 
-INSERT INTO book (id_book, title, author, publisher, isbn, id_category, images) VALUES
-    (5, 'Conscious', 'Annaka Harris', 'QuickRead', '947354211', 2, 'https://images-na.ssl-images-amazon.com/images/I/91Oy00Se3YL.jpg');
+INSERT INTO book (id_book, title, author, release_date, isbn, id_category, images) VALUES
+    (5, 'Conscious', 'Annaka Harris', '2021-09-17', '947354211', 2, 'https://images-na.ssl-images-amazon.com/images/I/91Oy00Se3YL.jpg');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,13 @@
 CREATE TABLE category(
     id_category SERIAL PRIMARY KEY,
-    name VARCHAR(100)
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE book(
     id_book SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     author VARCHAR(100) NOT NULL,
-    publisher VARCHAR(100),
-    release_date VARCHAR(20),
+    release_date DATE NOT NULL,
     isbn VARCHAR(20),
     id_category INT NOT NULL,
     FOREIGN KEY (id_category) REFERENCES category (id_category),
