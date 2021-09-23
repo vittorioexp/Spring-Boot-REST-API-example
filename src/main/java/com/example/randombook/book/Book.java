dfc76fd47131ddd2c76ef0a48ec7e10c7c949401
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-09-23 09:05:24 +0200

    writing comments for docs

--- src/main/java/com/example/randombook/book/Book.java
+++ src/main/java/com/example/randombook/book/Book.java
@@ -24,34 +24,74 @@ public class Book {
         this.images = images;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Gets author.
+     *
+     * @return the author
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Gets release date.
+     *
+     * @return the release date
+     */
     public Date getReleaseDate() {
         return releaseDate;
     }
 
+    /**
+     * Gets isbn.
+     *
+     * @return the isbn
+     */
     public String getIsbn() {
         return isbn;
     }
 
+    /**
+     * Gets category.
+     *
+     * @return the ID of the category
+     */
     public int getCategory() {
         return category;
     }
 
+    /**
+     * Gets images.
+     *
+     * @return the URLs of the images
+     */
     public String getImages() {
         return images;
     }
 
+    /**
+     * Gets the string representation of a book
+     *
+     * @return the string representation of a book
+     */
     @Override
     public String toString() {
         return "Book{" +
--- src/main/java/com/example/randombook/book/BookController.java
+++ src/main/java/com/example/randombook/book/BookController.java
@@ -16,37 +16,78 @@ public class BookController {
         this.dao = dao;
     }
 
+    /**
+     * Gets all books.
+     *
+     * @return the list of books
+     */
     @GetMapping
     public List<Book> findAll() {
         return dao.findAll();
     }
 
+    /**
+     * Gets books by category ID.
+     *
+     * @param id_category the category ID
+     * @return the books by category ID
+     */
     @GetMapping("/category/{id_category}")
     public List<Book> findAllByCategory(@PathVariable int id_category) {
         return dao.findAllByCategory(id_category);
     }
 
+    /**
+     * Gets a random book, optionally by category
+     *
+     * @param id_category the category ID
+     * @return a random book by category ID
+     */
     @GetMapping("/random")
     public Optional<Book> findRandomBook(@RequestParam(value = "category", defaultValue = "0") int id_category) {
         return dao.findRandomBook(id_category);
     }
 
+    /**
+     * Gets a book by ID
+     *
+     * @param id the book ID
+     * @return a book having the given ID
+     */
     @GetMapping("/{id}")
     public Optional<Book> findById(@PathVariable int id) {
         return dao.findById(id);
     }
 
+    /**
+     * Creates a book
+     *
+     * @param book the book to be created
+     * @return the just created book
+     */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Book create(@RequestBody Book book) {
         return dao.create(book);
     }
 
+    /**
+     * Updates a book
+     *
+     * @param book the book to be updated
+     * @param id the ID of the book to be updated
+     * @return the just updated book
+     */
     @PutMapping("/{id}")
     public Book update(@RequestBody Book book, @PathVariable int id) {
         return dao.update(book, id);
     }
 
+    /**
+     * Deletes a book
+     *
+     * @param id the ID of the book to be deleted
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         dao.delete(id);
